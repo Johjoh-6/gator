e19073236b2877973e16f72d565dd8242f6ff88c
Johann six <six.johann@gmail.com>
2026-03-12 12:36:25 +0100

    use sqlc for manage query and type of data

---
+++ sql/queries/feeds.sql
@@ -0,0 +1,19 @@
+-- name: CreateFeed :one
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6
+)
+RETURNING *;
+
+-- name: GetFeeds :many
+SELECT f.name, f.url, u.name as username
+FROM feeds f
+JOIN users u ON f.user_id = u.id;
+
+-- name: GetFeedByUrl :one
+SELECT * FROM feeds WHERE url = $1;
