266a799b8029df1661fbfabe493d9414e94d494c
Johann six <six.johann@gmail.com>
2026-03-12 12:37:11 +0100

    commands logic and type for manage the handler

---
+++ commands.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handlers[cmd.name]
+	if !ok {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	if c.handlers == nil {
+		c.handlers = make(map[string]func(*state, command) error)
+	}
+	c.handlers[name] = f
+}
